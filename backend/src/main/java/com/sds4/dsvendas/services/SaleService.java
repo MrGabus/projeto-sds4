919cc531007189fa82a8489d8c7a963dbf648502
Gabriel <gabrielsiodoni@hotmail.com>
2021-09-09 16:47:19 -0300

    Group by search

--- backend/src/main/java/com/sds4/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/sds4/dsvendas/services/SaleService.java
@@ -1,6 +1,8 @@
 package com.sds4.dsvendas.services;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sds4.dsvendas.dto.SaleDTO;
+import com.sds4.dsvendas.dto.SaleSuccessDTO;
+import com.sds4.dsvendas.dto.SaleSumDTO;
 import com.sds4.dsvendas.entities.Sale;
 import com.sds4.dsvendas.repositories.SaleRepository;
 import com.sds4.dsvendas.repositories.SellerRepository;
@@ -26,5 +30,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
